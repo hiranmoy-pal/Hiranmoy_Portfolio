d7224aed7a9165470e48e073e6c545a67072c6d9
Hiranmoy_Pal <palhiranmoy1999@gmail.com>
2024-02-12 18:47:35 +0530

    worked on Profile section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hiranmoy_Portfolio';
+
+  getResume() {
+    const pdfUrl = 'assets/pdf/CV_Hiranmoy.pdf';
+    this.downloadFile(pdfUrl, 'CV_Hiranmoy');
+  }
+
+  private downloadFile(url: string, fileName: string) {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a URL for the blob
+        const blobUrl = window.URL.createObjectURL(blob);
+
+        // Create a link element
+        const a = document.createElement('a');
+        a.href = blobUrl;
+        a.download = fileName;
+
+        // Append the link element to the body
+        document.body.appendChild(a);
+
+        // Trigger the click event on the link
+        a.click();
+
+        // Clean up: remove the link element and revoke the blob URL
+        window.URL.revokeObjectURL(blobUrl);
+        document.body.removeChild(a);
+      });
+  }
 }
