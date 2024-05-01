fb03e76d33be6dc8ebb7990bc269bf6d243974b7
Hiranmoy_Pal <palhiranmoy1999@gmail.com>
2024-05-02 01:43:53 +0530

    About, Service and skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hiranmoy_Portfolio';
   pdfUrl: any = 'assets/pdf/CV_Hiranmoy.pdf';
   blobUrl: any;
   a: any;
   aboutOpen: boolean = false;
+  servicesOpen: boolean = false;
+  skillsOpen: boolean = false;
+  educationOpen: boolean = false;
+  certificationOpen: boolean = false;
+  contactOpen: boolean = false;
 
   constructor(
   ) { }
+
+  ngOnInit(): void {
+
+    /****************** Multi-text *********************/
+
+    const typed = new Typed('.multiple-text', {
+      strings: ['Software Engineer 🧑‍💻', 'Frontend Developer 💻', 'Angular Developer'],
+      typeSpeed: 70,
+      backSpeed: 70,
+      backDelay: 1000,
+      loop: true
+    });
+  }
 
   /****************** Download Cv *********************/
 
@@ -54,6 +73,76 @@ export class AppComponent {
         const aboutSection = document.getElementById('about');
         if (aboutSection) {
           aboutSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }
+
+  /****************** Open Service *********************/
+
+  toggleServices() {
+    this.servicesOpen = !this.servicesOpen;
+    if (this.servicesOpen) {
+      setTimeout(() => {
+        const servicesSection = document.getElementById('services');
+        if (servicesSection) {
+          servicesSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }
+
+  /****************** Open Skills *********************/
+
+  toggleSkills() {
+    this.skillsOpen = !this.skillsOpen;
+    if (this.skillsOpen) {
+      setTimeout(() => {
+        const skillsSection = document.getElementById('skills');
+        if (skillsSection) {
+          skillsSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 400);
+    }
+  }
+
+  /****************** Open Education *********************/
+
+  toggleEducation() {
+    this.educationOpen = !this.educationOpen;
+    if (this.educationOpen) {
+      setTimeout(() => {
+        const educationSection = document.getElementById('education');
+        if (educationSection) {
+          educationSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }
+
+  /****************** Open Certification *********************/
+
+  toggleCertification() {
+    this.certificationOpen = !this.certificationOpen;
+    if (this.certificationOpen) {
+      setTimeout(() => {
+        const certificationSection = document.getElementById('certification');
+        if (certificationSection) {
+          certificationSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }
+
+  /****************** Open Contact *********************/
+
+  toggleContact() {
+    this.contactOpen = !this.contactOpen;
+    if (this.contactOpen) {
+      setTimeout(() => {
+        const contactSection = document.getElementById('contact');
+        if (contactSection) {
+          contactSection.scrollIntoView({ behavior: 'smooth' });
         }
       }, 100);
     }
